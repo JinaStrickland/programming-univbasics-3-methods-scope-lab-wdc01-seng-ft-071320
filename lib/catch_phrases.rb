2ecd8303d16ef16c1169837f59d3eb70a6cac17d
Jina Strickland <jina30@gmail.com>
2020-06-26 00:59:53 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,7 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  def status
+    puts 'Thank You Mario! But Our Princess Is In Another Castle!'
+  end
   phrase = "It's-a me, Mario!"
   puts phrase
 end
